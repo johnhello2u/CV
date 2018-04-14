e2e20d90fe8dd4405253d1f96b105349df6f3da3
johan_chou <john.hello2u@gmail.com>
2018-04-14 15:11:08 +0200

    2nd inced

--- index.php
+++ index.php
@@ -2,10 +2,36 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <?php require_once('other/connect.php'); ?>
+    <link rel="stylesheet" type="text/css" href="other/style.css">
+    <title>Homepage</title>
   </head>
   <body>
-    hello dude bdkdbkdbkdbkd
+
+admin login <form class="" action="connect.php" method="post">
+  <input type="text" name="usern" placeholder="username">
+  <input type="password" name="pass" placeholder="password">
+  <input type="submit" value="submit">
+</form>
+
+
+
+  <ul>
+    <li><a href="#/home">My jobs</a></li>
+    <li><a href="#/home">Education</a></li>
+    <li><a href="#/home">Languages</a></li>
+    <li><a href="#/home">programming skills</a></li>
+  </ul>
+
+
+
+<div>
+  <p>hello welcome to my page, my name is Johan</p>
+</div>
+
+<p>
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+</p>
     <div>
 
     </div>
